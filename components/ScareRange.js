a5f3255e3bf90ff9704465b1fb990bc1bdb964ba
Tyler D <tyler@tdaigle.com>
2021-10-06 14:01:03 -0400

    custom range input working

--- components/ScareRange.js
+++ components/ScareRange.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 import styles from "../styles/ScareRange.module.css";
 
 function ScareRange(props) {
+  const [scareLevel, setScareLevel] = useState(1);
+
   return (
     <div className={styles.scareRangeContainer}>
       <span className={styles.scareRangeLabel}>Scare Level</span>
+      <input
+        className={styles.scareRangeControl + " scareRange"}
+        type="range"
+        value={scareLevel}
+        min="1"
+        max="100"
+        onChange={(e) => setScareLevel(e.target.value)}
+        style={{
+          background: `linear-gradient(to right, red, black ${scareLevel}%)`,
+        }}
+      />
     </div>
   );
 }
